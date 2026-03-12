cfd0250d50cb87811fcbb54e2152603ce23553c7
kabo mekgwe <kabokm@gmail.com>
2026-03-12 18:51:23 +0200

    chore: wire up all modules in app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,32 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
+import { DatabaseModule } from './database/database.module';
+import { RedisModule } from './redis/redis.module';
+import { StripeModule } from './stripe/stripe.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { PaymentMethodsModule } from './payment-methods/payment-methods.module';
+import { PaymentsModule } from './payments/payments.module';
+import { UsageModule } from './usage/usage.module';
+import { WebhooksModule } from './webhooks/webhooks.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    ScheduleModule.forRoot(),
+    DatabaseModule,
+    RedisModule,
+    StripeModule,
+    AuthModule,
+    UsersModule,
+    PaymentMethodsModule,
+    PaymentsModule,
+    UsageModule,
+    WebhooksModule,
+  ],
 })
 export class AppModule {}
